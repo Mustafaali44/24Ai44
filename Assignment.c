70d7f151fc55fcd4090bfe2480b1f466ad898c07
Mustafaali44 <ma7082958@gmail.com>
2024-10-19 16:18:16 +0500

    Add files via upload

---
+++ Assignment.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+int a, b, sum;
+printf("enter two number");
+scanf("%d %d",&a,&b);
+sum =a+b;
+  printf("The sum of %d and %d is;%d\n",a,b,sum);
+    return 0;
+    
+}
