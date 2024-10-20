bb4f671992dade5112ca4089b86ce1bc1fe05016
Mustafaali44 <ma7082958@gmail.com>
2024-10-20 10:57:36 +0500

    Add files via upload

---
+++ Program 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+int Fahrenheit, Celsius;
+printf("enter temperature in Fahrenheit:");
+scanf("%d",&Fahrenheit);
+Celsius=(Fahrenheit-32)*5/9;
+  printf("Temperature in Celsius:%.2d\n");
+   return 0;
+    getch() ;
+    
+}
---
+++ program 2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+int Fahrenheit, Celsius;
+printf("enter temperature in Celsius:");
+scanf("%d",&Celsius);
+Fahrenheit=(Celsius*5/9)-32;
+  printf("Temperature in Fahrenheit:%.2d\n");
+   return 0;
+    getch() ;
+    
+}
